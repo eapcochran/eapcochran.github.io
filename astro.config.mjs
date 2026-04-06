4abe19c0e012680cb2174e1db07d8f66ffab1d3f
Ewen Cochran <eapcochran@gmail.com>
2026-04-06 09:10:04 -0400

    update deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ewen.dev',
+  site: 'https://eapcochran.github.io',
   integrations: [react()]
 });
